322e70710c902365dc6326204a863f5b39847ae4
Charles Carter <charles.h.carter@gmail.com>
2016-03-25 10:15:57 -0700

    create meal obj, meal-list component and beginning on main parent component

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,12 +1,28 @@
 import { Component } from 'angular2/core';
+import { Meal } from './meal.model';
 
 @Component({
   selector: 'my-app',
   template: `
-    <h1>Skeleton Angular2 App!</h1>
+  <div class="container">
+    <h1>SnackTrack</h1>
+    <meal-list
+     [mealList]="meals"
+     (onMealSelect)="mealWasSelected($event)">
+     </meal-list>
+   </div>
   `
 })
 
 export class AppComponent {
+  public meals: Meal[];
+  constructor() {
+    this.meals = [
+      new Meal("Poutine", "canada's contribution to american obesity", 2300),
+      new Meal("Cheese Curds", "a disgusting tradition from a fascinating place, wisconsin", 1200),
+      new Meal("Celery", "watery, crunchy, cud-inspiring", 12),
+      new Meal("Radicchio", "the 'blood cabbage', I tried a leaf", 60)
+    ];
+  }
 
 }
